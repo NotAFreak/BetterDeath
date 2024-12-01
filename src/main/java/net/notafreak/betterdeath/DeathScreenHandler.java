9bcddb8256c4d4728ac5962b2bc907a1df9fb676
NotAFreak <oskar.kington1102@gmail.com>
2024-12-01 22:35:23 +1000

    Revert "Merge branch 'main' of https://github.com/NotAFreak/BetterDeath"
    This reverts commit d74a99eca2f96dbb81d933880250532e02e5742a, reversing
    changes made to d931510d42f7b4f7256b93b8fa03e80606788f3f.

--- src/main/java/net/notafreak/betterdeath/DeathScreenHandler.java
+++ src/main/java/net/notafreak/betterdeath/DeathScreenHandler.java
@@ -27,10 +27,11 @@ public class DeathScreenHandler {
     @SubscribeEvent
     public static void onRenderOverlay(RenderGuiOverlayEvent.Pre event) {
         if (deathScreenTimer > 0) {
+            
             GuiGraphics guiGraphics = event.getGuiGraphics();
             RenderSystem.enableBlend();
             RenderSystem.defaultBlendFunc();
-            
+
             // Draw a black rectangle across the entire screen
             int screenWidth = Minecraft.getInstance().getWindow().getGuiScaledWidth();
             int screenHeight = Minecraft.getInstance().getWindow().getGuiScaledHeight();
@@ -50,6 +51,12 @@ public class DeathScreenHandler {
             guiGraphics.pose().translate(0, 0, -9000);
             
             RenderSystem.disableBlend();
+            
+            // in the future i want to try and get this only executing once instead of each render update
+            Minecraft.getInstance().getSoundManager().pause();
+        } else {
+            // sounds may continue
+            Minecraft.getInstance().getSoundManager().resume();
         }
     }
 
