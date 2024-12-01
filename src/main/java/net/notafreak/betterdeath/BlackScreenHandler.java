8056b9f7380b00a18e26595b2e1c523651fff22f
NotAFreak <oskar.kington1102@gmail.com>
2024-12-01 11:02:12 +1000

    refactoring stuff

--- src/main/java/net/notafreak/betterdeath/BlackScreenHandler.java
+++ src/main/java/net/notafreak/betterdeath/BlackScreenHandler.java
@@ -4,11 +4,12 @@ import net.minecraftforge.client.event.RenderGuiOverlayEvent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.notafreak.betterdeath.config.ClientConfig;
 
 import com.mojang.blaze3d.systems.RenderSystem;
-
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.server.level.ServerPlayer;
 
 @Mod.EventBusSubscriber(modid = "betterdeath", bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class BlackScreenHandler {
@@ -33,13 +34,26 @@ public class BlackScreenHandler {
             // Draw a black rectangle across the entire screen
             int screenWidth = Minecraft.getInstance().getWindow().getGuiScaledWidth();
             int screenHeight = Minecraft.getInstance().getWindow().getGuiScaledHeight();
-            guiGraphics.fill(0, 0, screenWidth, screenHeight, 0xAA000000); // 0xAA for semi-transparency
+            guiGraphics.fill(0, 0, screenWidth, screenHeight, ConstructColorHex(ClientConfig.deathScreenR.get(), ClientConfig.deathScreenG.get(), ClientConfig.deathScreenB.get(), 255)); // 0xAA for semi-transparency
             
             RenderSystem.disableBlend(); // Disable blending when done
         }
     }
 
-    public static void triggerBlackScreen() {
+    // Construct color hexcode following ARGB
+    public static int ConstructColorHex(int R, int G, int B, int A) {
+        // guys this is so fucked
+        R = Math.max(0, Math.min(255, R));
+        G = Math.max(0, Math.min(255, G));
+        B = Math.max(0, Math.min(255, B));
+        A = Math.max(0, Math.min(255, A));
+
+        // Combine the ARGB components into a single integer
+        return (A << 24) | (R << 16) | (G << 8) | B;
+    } 
+
+    public static void triggerBlackScreen(ServerPlayer player) {
         blackScreenTimer = BLACK_SCREEN_DURATION;
+        // player is invincible whilst black screen
     }
 }
