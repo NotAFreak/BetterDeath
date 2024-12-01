a90f36391b8a19e6d66b8e34a4d987e63b1a3edb
NotAFreak <oskar.kington1102@gmail.com>
2024-12-01 11:02:12 +1000

    refactoring stuff

--- src/main/java/net/notafreak/betterdeath/BetterDeath.java
+++ src/main/java/net/notafreak/betterdeath/BetterDeath.java
@@ -5,28 +5,28 @@ package net.notafreak.betterdeath;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraft.server.level.ServerLevel;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.level.GameType;
 import net.minecraft.world.phys.Vec3;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
+
 import net.minecraft.core.BlockPos;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.notafreak.betterdeath.config.ClientConfig;
+import net.notafreak.betterdeath.config.CommonConfig;
+
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(BetterDeath.MODID)
-public class BetterDeath
-{
-    // Define mod id in a common place for everything to reference
+public class BetterDeath {
+    public static final String NAME = "Better Death";
     public static final String MODID = "betterdeath";
     private static final Logger LOGGER = LogUtils.getLogger();
 
@@ -34,12 +34,18 @@ public class BetterDeath
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
+        modEventBus.addListener(this::clientSetup);
+
+        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, ClientConfig.SPEC, MODID + "-client.toml");
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, CommonConfig.SPEC, MODID + "-common.toml");
         
         MinecraftForge.EVENT_BUS.register(this);
     }
 
+    private void clientSetup(FMLClientSetupEvent event) {
+        // we do fuck all lol
+    }
     private void commonSetup(final FMLCommonSetupEvent event)
     {
 
@@ -64,28 +70,13 @@ public class BetterDeath
             Vec3 respawnPos = new Vec3(pos.getX(), pos.getY(), pos.getZ());
             player.teleportTo(player.serverLevel(), respawnPos.x, respawnPos.y, respawnPos.z + 1, 0, 0);
             LOGGER.info("respawned player");
-            BlackScreenHandler.triggerBlackScreen();
+            BlackScreenHandler.triggerBlackScreen(player);
         }
     }
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event)
     {
-        // Do something when the server starts
-        LOGGER.info("HELLO from server starting");
-    }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents
-    {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
-            // Some client setup code
-            LOGGER.info("HELLO FROM CLIENT SETUP");
-            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
-        }
     }
 }
