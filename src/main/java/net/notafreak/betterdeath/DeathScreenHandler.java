e20dda40183d64a3e059d53c4727aed2f4e9edaa
NotAFreak <oskar.kington1102@gmail.com>
2025-04-26 18:39:34 +1000

    The server now tells the client what happened. It finally fully works.

--- src/main/java/net/notafreak/betterdeath/DeathScreenHandler.java
+++ src/main/java/net/notafreak/betterdeath/DeathScreenHandler.java
@@ -13,13 +13,18 @@ import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.notafreak.betterdeath.config.ClientConfig;
+import net.notafreak.betterdeath.config.CommonConfig;
+import net.notafreak.betterdeath.network.PacketHandler;
+import net.notafreak.betterdeath.network.S2CdeathNotifyPacket;
 
 @Mod.EventBusSubscriber(modid = "betterdeath", bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class DeathScreenHandler {
     
     public static final Map<String, AffectedPlayerData> affectedPlayers = new HashMap<>(); // Username, data
 
-    @SubscribeEvent
+    
+
+    /*@SubscribeEvent
     public static void onPlayerRespawn(TickEvent.PlayerTickEvent event) {
         if (event.player.level().isClientSide()) return; // Only process on the server side
         String username = event.player.getName().getString();
@@ -28,6 +33,18 @@ public class DeathScreenHandler {
             data.deathScreenTimer--;
             if (data.deathScreenTimer <= 0) {
                 affectedPlayers.remove(username); // Remove the player if timer ends
+            }
+        }
+    }*/
+    @SubscribeEvent
+    public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
+        String username = event.player.getName().getString();
+        AffectedPlayerData data = affectedPlayers.get(username);
+    
+        if (data != null) {
+            data.deathScreenTimer--;
+            if (data.deathScreenTimer <= 0) {
+                affectedPlayers.remove(username);
             }
         }
     }
@@ -79,8 +96,22 @@ public class DeathScreenHandler {
         return (A << 24) | (R << 16) | (G << 8) | B;
     }
 
+    // Used for server
     public static void triggerDeathScreen(ServerPlayer player) {
         affectedPlayers.put(player.getName().getString(), new AffectedPlayerData());
-        
+        PacketHandler.sendToPlayer(new S2CdeathNotifyPacket(CommonConfig.deathScreenDuration.get()), player);
+    }
+
+    // Used for client
+    public static void triggerDeathScreen(float duration) {
+        Minecraft mc = Minecraft.getInstance();
+        if(mc.player == null) {
+            return;
+        }
+
+        String username = mc.player.getName().getString();
+        AffectedPlayerData data = new AffectedPlayerData();
+        data.deathScreenTimer = (int) duration;
+        affectedPlayers.put(username, data);
     }
 }
