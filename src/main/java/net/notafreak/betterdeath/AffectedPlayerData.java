d04a21eb8353e2eb4316af5cc9ca5793ed286fca
007_Special <007Special4u@gmail.com>
2025-05-02 23:03:23 +0200

    entirely prevented the player from moving while in blackscreen

--- src/main/java/net/notafreak/betterdeath/AffectedPlayerData.java
+++ src/main/java/net/notafreak/betterdeath/AffectedPlayerData.java
@@ -2,11 +2,13 @@ package net.notafreak.betterdeath;
 
 
 import net.minecraft.world.level.GameType;
+import net.minecraft.world.phys.Vec3;
 import net.notafreak.betterdeath.config.CommonConfig;
 
 public class AffectedPlayerData {
     public int deathScreenTimer = 0; // In game ticks
     public GameType previousGameType = GameType.SURVIVAL; // Just default
+    public Vec3 respawnPos;
     
     public AffectedPlayerData(GameType prevGameType) {
         if(prevGameType == null) {
