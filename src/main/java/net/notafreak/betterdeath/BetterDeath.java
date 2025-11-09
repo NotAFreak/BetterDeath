faaecabfe62b0c8ea3cf279ed9687e640948db9b
NotAFreak <oskar.kington1102@gmail.com>
2025-11-09 11:37:35 +1000

    Freeze the player now

--- src/main/java/net/notafreak/betterdeath/BetterDeath.java
+++ src/main/java/net/notafreak/betterdeath/BetterDeath.java
@@ -3,7 +3,7 @@ package net.notafreak.betterdeath;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
-import net.fabricmc.fabric.api.event.server.ServerStartCallback;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
@@ -15,24 +15,29 @@ import net.notafreak.betterdeath.network.ServerPacketHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-// man i love how clean fabric's initialization and general code is.
 public class BetterDeath implements ModInitializer {
 	public static final String MOD_ID = "betterdeath";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	// man i love how clean fabric's initialization and general code is.
 	@Override
 	public void onInitialize() {
 		CommonModConfig.Register();
 		CommonModConfig.Load();
 		ServerPacketHandler.RegisterC2Spackets();
-		
 		ServerLifecycleEvents.SERVER_STARTED.register(this::onServerStart);		
+		ServerTickEvents.END_SERVER_TICK.register(this::onServerTick);
 		ServerLivingEntityEvents.AFTER_DEATH.register(this::onDeath);
 	}
 
 	private void onServerStart(MinecraftServer server) {
 		if(CommonModConfig.config.forceImmediateRespawn) {
 			server.getGameRules().get(GameRules.DO_IMMEDIATE_RESPAWN).set(true, server);
+		}
+	}
+	private void onServerTick(MinecraftServer server) {
+		for(ServerPlayerEntity player : server.getPlayerManager().getPlayerList()) {
+			ServerDeathScreenHandler.onPlayerTick(player);
 		}
 	}
 	private void onDeath(LivingEntity entity, DamageSource source) {
@@ -43,7 +48,6 @@ public class BetterDeath implements ModInitializer {
 			}
 			return;
 		}
-		ServerPlayerEntity player = (ServerPlayerEntity)entity;
-		ServerPacketHandler.sendDeathNotify(player, CommonModConfig.config.deathScreenDuration);
+		ServerDeathScreenHandler.Trigger((ServerPlayerEntity)entity);
 	}
 }
--- src/main/java/net/notafreak/betterdeath/ServerDeathScreenHandler.java
+++ src/main/java/net/notafreak/betterdeath/ServerDeathScreenHandler.java
@@ -1,8 +1,39 @@
 package net.notafreak.betterdeath;
-
 import java.util.HashMap;
 import java.util.Map;
 
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.world.GameMode;
+import net.notafreak.betterdeath.network.ServerPacketHandler;
+
 public class ServerDeathScreenHandler {
     public static final Map<String, AffectedPlayerData> affectedPlayers = new HashMap<>();
+
+    // This is only called by the server
+    public static void onPlayerTick(ServerPlayerEntity player) {
+        if(affectedPlayers.size() == 0) return;
+        String username = player.getName().getString();
+        AffectedPlayerData data = affectedPlayers.get(username);
+        if(data == null) return;
+
+        // Force the player to not be able to move
+        if(data.respawnPos != null) {
+            player.teleport(data.respawnPos.x, data.respawnPos.y, data.respawnPos.z);
+        }
+        data.deathScreenTimer--;
+        if (data.deathScreenTimer <= 0) {
+            player.changeGameMode(data.previousGameType);
+            BetterDeath.LOGGER.info("Reset to previous gamemode (" + data.previousGameType + ")");
+            affectedPlayers.remove(username);
+        }
+    }
+
+    public static void Trigger(ServerPlayerEntity player) {
+        AffectedPlayerData data = new AffectedPlayerData(player.interactionManager.getGameMode());
+        data.respawnPos = player.getSpawnPointPosition().toCenterPos(); // This is under the idea that this code gets called AFTER the player dies
+        data.deathScreenTimer = CommonModConfig.config.deathScreenDuration;
+        affectedPlayers.put(player.getName().getString(), data);
+        player.changeGameMode(GameMode.SPECTATOR);
+        ServerPacketHandler.sendDeathNotify(player, CommonModConfig.config.deathScreenDuration);
+    }
 }
